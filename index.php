8e7cfd6d31be2e06ece6dfad7f8112cc53ea35e6
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-04-29 17:32:41 -0400

    added order form and summary

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+session_start();
 
 require_once('vendor/autoload.php');
 
@@ -24,6 +25,32 @@ $f3->route('GET /breakfast ', function(){
 $f3->route('GET /breakfast/green-eggs ', function(){
     $view = new Template();
     echo $view->render('views/greenEggsAndHam.html');
+});
+
+$f3->route('GET|POST /order ', function($f3){
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        var_dump($_POST);
+        $meals = array("breakfast", "lunch", "dinner");
+
+        if(empty($_POST['food']) || !in_array($_POST['meal'], $meals)){
+            echo "<p>Please enter a food</p>";
+        } else{
+            $_SESSION['food'] = $_POST['food'];
+            $_SESSION['meal'] = $_POST['meal'];
+            $f3->reroute('summary');
+            session_destroy();
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('views/order.html');
+
+});
+
+$f3->route('GET /summary ', function(){
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 $f3-> run();
